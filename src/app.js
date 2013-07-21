2d907212a5ee912ce2f4e153d5caa1886005ed3f
lorenzofox3 <laurent34azerty@gmail.com>
2013-07-21 21:10:17 +0200

    first  documentation commit

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.controller('mainCtrl', ['$scope', function (scope) {
             restrict: 'E',
             link: function (scope, element, attr, ctrl) {
 
-                var index = attr.index,
+                var index = parseInt(attr.index,10),
                     minWidth = attr.minWidth || 35,
                     maxWidth = attr.maxWidth || 200;
 
@@ -23,7 +23,7 @@ app.controller('mainCtrl', ['$scope', function (scope) {
                 };
 
                 scope.$watch('isCollapsed', function (newValue, oldValue) {
-                    if (newValue!==oldValue) {
+                    if (newValue !== oldValue) {
                         var newLength = newValue === true ? minWidth - element.parent()[0].offsetWidth : maxWidth - element.parent()[0].offsetWidth;
                         ctrl.moveBlockLength(index, newLength);
                         ctrl.toggleLockBlock(index, true);
@@ -31,19 +31,33 @@ app.controller('mainCtrl', ['$scope', function (scope) {
                 });
             }
         };
-    });
+    })
+    .directive('blockCommand', function () {
+        return {
+            restrict: 'C',
+            require: '^flexyLayout',
+            template: '<div><input type="number" ng-model="newLength"/><input type="checkbox" ng-model="isLocked"/></div>',
+            replace: true,
+            scope: {},
+            link: function (scope, element, attrs, ctrl) {
+                var index = parseInt(attrs.index,10);
 
-//var app = angular.module('app', ['flexyLayout.mediator']);
-//app.controller('mainCtrl', ['$scope', function (scope) {
-//        scope.greetings = "hello";
-//    }]).directive('collapse', function () {
-//        return {
-//            require: '^flexyLayout',
-//            replace: true,
-//            scope: {},
-//            template: '<button ng-click="toggle()">test</button>',
-//            restrict: 'E',
-//
-//        };
-//    });
+                scope.isLocked=false;
+
+                var input=angular.element(element.children()[1]);
+                input.bind('blur', function () {
+                    scope.$apply(function () {
+                        ctrl.moveBlockLength(index,scope.newLength);
+                    });
+                });
+
+                scope.$watch('isLocked', function (newValue, oldValue) {
+                    if (newValue !== oldValue) {
+                        ctrl.toggleLockBlock(index, newValue);
+                    }
+                });
+            }
+
+        };
+    });
 
